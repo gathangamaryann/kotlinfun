bc79c84e8c19f62eca802e6b1da33ca574b125e4
gathangamaryann <maryanngathanga@gmail.com>
2022-02-19 16:13:00 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,24 @@
+fun main(){
+name()
+    modulus(5 , 2)
+    addition(1, 2,3,4)
+    intrestingfact()
+}
+fun name(){
+    var name = "maryann"
+    println("hello" +name)
+}
+fun modulus(a:Int , b:Int){
+    var modulus = a % b
+    println(modulus)
+}
+fun addition(a:Int , b:Int , c:Int , d:Int){
+    var addition = (a+b+c+d)
+    println(addition)
+
+}
+fun intrestingfact(){
+    var fact="i write poetry"
+    println(fact)
+}
+
